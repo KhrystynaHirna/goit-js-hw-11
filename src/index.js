70872bb774ea5949659d6369a4069938e78268d1
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-02-20 00:57:57 +0100

    add options

--- src/index.js
+++ src/index.js
@@ -8,19 +8,37 @@ const searchButton = document.querySelector('form button');
 const BASE_URL = 'https://pixabay.com/api/?key=25789368-636c5d004c25f97cc91a0f5f4';
 
 input.addEventListener('input', onInputForm);
-searchButton.addEventListener('submit', onSearchButton);
+searchButton.addEventListener('click', onSearchButton);
 
 // const userSearch = onInputForm();
 let userSearch = '';
 
+const options = {
+    key: '25789368-636c5d004c25f97cc91a0f5f4',
+    body: JSON.stringify(),
+    q: evt.currentTarget.value,
+    image_type: photo,
+    orientation: horizontal,
+    safesearch: true,
+    headers: {
+        "Content-Type": "application/json; charset=UTF-8",
+    }   
+};
+
 const searchImages = async() => {
-    const response = await fetch(`${BASE_URL}&q=${userSearch}&image_type=photo`);
+    const response = await fetch(`${BASE_URL}&q=${userSearch}&image_type`);
     const images = await response.json();
+    return images;
 }
+searchImages().then(images => console.log(images));
 
-function onInputForm() {
+function onInputForm(evt) {
+    userSearch = evt.currentTarget.value;
+  
+};
 
-}
-function onSearchButton() {
-
+function onSearchButton(evt) {
+    // evt.preventDefault();
+  // maje zapustytys poszuk searchImages
+    // tut maje povernutys searchImages
 }
