b856bbf0b43cb3f4d0c835a7aac8910e15ce8345
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-04-01 02:06:21 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -16,43 +16,41 @@ searchEl.addEventListener('submit', onFormSubmit);
 loadMoreButton.addEventListener('click', onLoadMoreButton);
 loadMoreButton.hidden = true;
 
-async function onFormSubmit(e) {
+
+function onFormSubmit(e) {
     e.preventDefault();
     loadMoreButton.hidden = true;
+    //  te szczo wwodytymut korystuwaczi
     imagesApiService.query = e.currentTarget.elements.searchQuery.value.trim();
 
-     if (imagesApiService.query === '') {
+    if (imagesApiService.query === '') {
         return Notiflix.Notify.warning("Please enter something.");
     }
     clearGalleryImagesMarkup();
     imagesApiService.resetPage();
 
-    try {
-        const responseImages = await imagesApiService.fetchImages().then(galleryImagesMarkup);
-        const input = responseImages;
-
-        if (input === 0) {
-           return Notiflix.Notify.warning("Sorry, there are no images matching your search query. Please try again.");
+    imagesApiService.fetchImages().then(pictures => {
+        if (pictures.data.totalHits !== 0 && pictures.data.hits.length !== 0) {
+            Notiflix.Notify.success(`Hooray! We found ${imagesApiService.query} images.`,
+            );
+            loadMoreButton.hidden = false;
         } else {
-            Notiflix.Notify.success(`Hooray! We found ${imagesApiService.query} images.`);
+            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
         }
-
-        loadMoreButton.hidden = false;
-        galleryImagesMarkup();
-        //  onLoadMoreButton();
-    }
-    catch (error) {
-        console.log(error);
-    }
-    let lightbox = new SimpleLightbox('.photo-card a', { captionsData: 'alt', captionDelay: 250 });
-    lightbox.refresh();
-};
+  
+        galleryImagesMarkup(pictures);
+        let lightbox = new SimpleLightbox('.gallery a', {
+                captionsData: 'alt',
+                captionDelay: 250
+            });
+    });
+}
 
 function onLoadMoreButton() {
     imagesApiService.fetchImages().then(galleryImagesMarkup);
 }
-function galleryImagesMarkup(responseImages) {
-    imagesGallery.insertAdjacentHTML('beforeend', photoCards(responseImages));
+function galleryImagesMarkup(pictures) {
+    imagesGallery.insertAdjacentHTML('beforeend', photoCards(pictures));
 }
 function clearGalleryImagesMarkup() {
     imagesGallery.innerHTML = '';
