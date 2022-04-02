041a2d06fcc3b3888956eff64b21e131a3e0e2d1
KhrystynaHirna <86721411+KhrystynaHirna@users.noreply.github.com>
2022-04-02 02:53:04 +0200

    update all

--- src/js/images-service.js
+++ src/js/images-service.js
@@ -1,34 +1,16 @@
 import axios from "axios";
 
-export default class ImagesApiService {
-    constructor() {
-        this.userSearch = '';
-        this.page = 1;
-        this.per_page = 40;
-    }
-    // robymo zapyt na kartynky metodom get
-   async fetchImages () {
-       try {
-           const BASE_URL = 'https://pixabay.com/api/';
-           const API_KEY = '25789368-636c5d004c25f97cc91a0f5f4';
-           const parameters = `lang=en&image_type=photo&orientation=horizontal&safesearch=true&page=${this.page}&per_page=${this.per_page}`;
-           const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${this.userSearch}&${parameters}`);   
-            this.incrementPage();
-            return response;
-        } catch (error) {
-        return Error;
-        }  
-    }
-        incrementPage() {
-            this.page += 1;
-        }
-        resetPage() {
-            this.page = 1;
-        }
-    get query() {
-            return this.userSearch;
-        }
-    set query(newUserSearch) {
-            this.userSearch = newUserSearch;
-        }
+const BASE_URL = 'https://pixabay.com/api/';
+const API_KEY = '25789368-636c5d004c25f97cc91a0f5f4';
+
+const per_page = 40;
+const parameters = `lang=en&image_type=photo&orientation=horizontal&safesearch=true&per_page=${per_page}`;
+
+export const getImages = async (query, page) => {
+  try {
+  const response = await axios.get(`${BASE_URL}?key=${API_KEY}&q=${query}&${parameters}&page=${page}`);
+  return await response.data;
+  } catch (error) {
+    console.log(error);
+  }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,61 +4,93 @@ import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import Notiflix from 'notiflix';
 import photoCards from '../photo_card.hbs';
-import ImagesApiService from './images-service';
+import { getImages } from './images-service';
 
 const searchEl = document.querySelector('.search-form');
 const imagesGallery = document.querySelector('.gallery');
 const loadMoreButton = document.querySelector('.load-more');
-
-const imagesApiService = new ImagesApiService();
+let queryInput = '';
+let page = 1;
+let timerId = null;
 
 searchEl.addEventListener('submit', onFormSubmit);
+searchEl.addEventListener('input', onInput);
 loadMoreButton.addEventListener('click', onLoadMoreButton);
 loadMoreButton.hidden = true;
-
-let lightbox = new SimpleLightbox('.gallery a', {
-                captionsData: 'alt',
-                captionDelay: 250
-        });
 
 function onFormSubmit(e) {
     e.preventDefault();
     loadMoreButton.hidden = true;
-    //  te szczo wwodytymut korystuwaczi
-    imagesApiService.query = e.currentTarget.elements.searchQuery.value.trim();
-
-    if (imagesApiService.query === '') {
-        return Notiflix.Notify.warning("Please enter something.");
-    }
     clearGalleryImagesMarkup();
-    imagesApiService.resetPage();
 
-    // te szczo korystuwaczi otrymajut
-    imagesApiService.fetchImages().then(pictures => {
-        if (pictures.data.totalHits !== 0 && pictures.data.hits.length !== 0) {
-            Notiflix.Notify.success(`Hooray! We found ${pictures.data.totalHits} images.`);
-            loadMoreButton.hidden = false;
-            lightbox.refresh();
-        } else {
-            Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-            return;
-        }
-            if (pictures.data.totalHits < 40) {
-                Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.");
-                loadMoreButton.hidden = true;
-            }
+if (queryInput !== '') {
+        getImages(queryInput)
+        .then(createGallery)
+        .catch(noResultsMessage);
+    }
+    page = 1;
+}
+function onInput(e) {
+    queryInput = e.target.value.trim();
+}
+function createGallery(images) {
+    clearInterval(timerId);
+    if (images.totalHits === 0) {
+            noResultsMessage(images);
+    } else {
+        galleryImagesMarkup(images);
+        loadMoreButton.hidden = false;
         
-        galleryImagesMarkup(pictures);
-        lightbox.refresh();
-    });
+        if (images.hits.length < 40) {
+            endOfListMessage();
+            loadMoreButton.hidden = true;
+          }
+        }
+        if (page === 1 & images.totalHits > 0) {
+        resultsMessage(images);
+        }
 }
 function onLoadMoreButton() {
-    imagesApiService.fetchImages().then(galleryImagesMarkup);
-     
+    page += 1;
+    getImages(queryInput, page)
+        .then(images => {
+            createGallery(images);
+            smoothScroll();
+        })
+        .catch(noResultsMessage);
 }
-function galleryImagesMarkup(pictures) {
-    imagesGallery.insertAdjacentHTML('beforeend', photoCards(pictures));
+function smoothScroll() {
+  const { height: cardHeight } = imagesGallery 
+  .firstElementChild.getBoundingClientRect();
+
+  window.scrollBy({
+  top: cardHeight * 2,
+  behavior: 'smooth',
+  });  
+}
+function resultsMessage(images) {
+    Notiflix.Notify.success(`Hooray! We found ${images.totalHits} images.`);
+}
+function noResultsMessage() {
+    Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+}
+function endOfListMessage() {
+    timerId = setTimeout(() => {
+        Notiflix.Notify.warning("We're sorry, but you've reached the end of search results.")
+    }, 800)
+}
+function galleryImagesMarkup(images) {
+    images.hits.map(image => {
+    imagesGallery.insertAdjacentHTML('beforeend', photoCards(image));
+    })
+    let lightbox = new SimpleLightbox('.photo-card a');
+    lightbox.on('show.simplelightbox', function () { });
+    lightbox.refresh();
 }
 function clearGalleryImagesMarkup() {
     imagesGallery.innerHTML = '';
-};
+}
+
+
+
+
